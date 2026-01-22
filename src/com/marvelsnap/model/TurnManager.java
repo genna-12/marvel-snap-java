40d95eaca151a27bff30522c9412b551b9f17b08
Matteo Del Moro <matteo.delmoro@studio.unibo.it>
2026-01-22 18:59:36 +0100

    Aggiunge struttura di base per classe TurnManager

--- src/com/marvelsnap/model/TurnManager.java
+++ src/com/marvelsnap/model/TurnManager.java
@@ -1,5 +1,27 @@
 package com.marvelsnap.model;
 
 public class TurnManager {
+    private int currentTurn = 1;
+    private int currentPlayerIndex = 0;
+    private int maxTurns = 0;
+
+    public int getEnergyForTurn() {
+        return 0;
+    }
+
+    public void nextTurn() {
+        return;
+    }
+
+    public void switchPlayer() {
+        return;
+    }
     
+    public int getCurrentPlayerIndex() {
+        return 0;
+    }
+    
+    public boolean isTurnCycleComplete() {
+        return false;
+    }
 }
