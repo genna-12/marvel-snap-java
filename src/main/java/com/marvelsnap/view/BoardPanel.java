a0415c5cc293462214fda6b2fbdb90bf892d5daf
spazzolipaolo <paolo.spazzoli@studio.unibo.it>
2026-02-09 18:34:08 +0100

    Implementa classi BoardPanel e LocationPanel

--- src/main/java/com/marvelsnap/view/BoardPanel.java
+++ src/main/java/com/marvelsnap/view/BoardPanel.java
@@ -7,7 +7,6 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import com.marvelsnap.model.Location;
-import javax.swing.border.Border;
 
 public class BoardPanel extends JPanel {
 
